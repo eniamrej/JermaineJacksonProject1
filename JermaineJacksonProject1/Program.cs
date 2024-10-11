d251f516560d703be695d35bd77853f86edc3103
Jermaine Jackson <jj00490@my.westga.edu>
2024-10-11 15:13:49 -0400

    Added a message stating I successfully connected my project to the git repository

--- JermaineJacksonProject1/Program.cs
+++ JermaineJacksonProject1/Program.cs
@@ -12,6 +12,8 @@ namespace JermaineJacksonProject1
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            Console.WriteLine("Git repository successfully connected!");
         }
     }
 }
